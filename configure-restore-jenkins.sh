4993258f942e1b7ffe2b12a32d95ab08956e56bb
moosebeandev <bmccown93@gmail.com>
2018-11-14 20:32:12 -0600

    Adding conditional for restore script

--- configure-restore-jenkins.sh
+++ configure-restore-jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo chmod 755 /scripts/restore-jenkins.sh
-sudo sed -i s/BUCKETNAME/$BUCKET_NAME/g /scripts/restore-jenkins.sh
-echo "@reboot root /scripts/restore-jenkins.sh &" | sudo tee -a /etc/crontab
+if [ $RESTORE_JENKINS = true ]
+then
+	sudo chmod 755 /scripts/restore-jenkins.sh
+	sudo sed -i s/BUCKETNAME/$BUCKET_NAME/g /scripts/restore-jenkins.sh
+	echo "@reboot root /scripts/restore-jenkins.sh &" | sudo tee -a /etc/crontab
+fi
--- restore-jenkins.sh
+++ restore-jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo aws s3 sync s3://BUCKETNAME/jenkins /var/lib/jenkins
-sudo chmod 755 -R /var/lib/jenkins
-sudo chown jenkins:jenkins -R /var/lib/jenkins
+if [ $RESTORE_JENKINS = true ]
+then
+	sudo aws s3 sync s3://BUCKETNAME/jenkins /var/lib/jenkins
+	sudo chmod 755 -R /var/lib/jenkins
+	sudo chown jenkins:jenkins -R /var/lib/jenkins
+fi
